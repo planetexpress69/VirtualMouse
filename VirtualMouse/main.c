dd56feb0c022a8998a7de09c849f203c8b75bd6b
Martin Kautz <kautz@jakota.de>
2014-12-18 09:09:17 +0100

    Initial Commit

---
+++ VirtualMouse/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  VirtualMouse
+//
+//  Created by Martin Kautz on 18.12.14.
+//  Copyright (c) 2014 JAKOTA Design Group. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
