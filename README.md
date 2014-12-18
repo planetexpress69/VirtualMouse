270fb9cdeec9142a77deb1ea612e4daf3895a27e
Martin Kautz <kautz@jakota.de>
2014-12-18 12:29:11 +0100

    Finished prototype code.

---
+++ README.md
@@ -0,0 +1,18 @@
+VirtualMouse
+================
+
+A tiny program to take over your mouse and move over every pixel in a given rectangle.
+Made for my co-workers who are trying to make a big matrix using JavaScript to connect the pixels of a given webcam picture to geocoordinates.
+So far, there's no boundary checking and no way to get out of the running program. :-)
+
+
+Environments in which to run Ranglistenpunkte
+---------------------------------------------
+
+VirtualMouse runs on a Mac with Mac OS X. 
+
+
+Questions?
+----------
+
+If you have any questions, please feel free to get in touch with me and send your inquiries to support@jakota.de.
--- VirtualMouse/main.c
+++ VirtualMouse/main.c
@@ -6,10 +6,40 @@
 //  Copyright (c) 2014 JAKOTA Design Group. All rights reserved.
 //
 
-#include <stdio.h>
+#include <ApplicationServices/ApplicationServices.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+
+    if (argc != 5) {
+        printf ("Usage: mouse2 strtX strtY endX endY!\n");
+        exit(1);
+    }
+
+    int strtX = atoi(argv[1]);
+    int strtY = atoi(argv[2]);
+    int stopX = atoi(argv[3]);
+    int stopY = atoi(argv[4]);
+
+    int x;
+    int y;
+
+    sleep(5); // time to get the window with the picture in it
+
+    CGEventRef move = NULL;
+
+    for (y = strtY; y < stopY; y ++) {
+        for (x = strtX; x < stopX; x ++) {
+            move = CGEventCreateMouseEvent(
+                                           NULL,
+                                           kCGEventMouseMoved,
+                                           CGPointMake(x, y),
+                                           kCGMouseButtonLeft);
+            CGEventPost(kCGHIDEventTap, move);
+            CFRelease(move);
+            usleep(500000); // sleep for half a second to give the JavaScript some time...
+        }
+    }
+
+    exit (0);
 }
+
